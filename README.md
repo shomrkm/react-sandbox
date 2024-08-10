2829df47bebf22a7915a924e1abe1c578593f9b7
Shotaro Murakami <shochan1985@gmail.com>
2024-08-10 15:35:40 +0900

    feat: first settings.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is my react sandbox environment.
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      React Sandbox
+    </>
+  )
+}
+
+export default App
