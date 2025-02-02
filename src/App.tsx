100c409eeed963dcdf1923d5338591178e7b4587
shomrkm <shochan1985@gmail.com>
2025-02-02 16:12:26 +0900

    refact: adjust layouts

--- src/App.tsx
+++ src/App.tsx
@@ -28,16 +28,16 @@ function App() {
 
   return (
     <div className='container mx-auto p-10'>
-      <div className='flex gap-4 justify-between items-start mb-10'>
+      <div className='flex gap-12 justify-between items-start mb-10'>
         <div className='flex-col justify-start items-start border border-gray-200 p-4 rounded-md w-1/2'>
           <img src={`${BASE_URL}/images/1.png`} alt={data.name} style={{ width: '', objectFit: 'cover' }} />
           <div className='text-gray-500 text-3xl mt-2'>{data.name}</div>
         </div>
         <div className='flex-col justify-center items-center w-1/2'>
-          <Button onClick={handleAddToCart} className='w-full bg-yellow-300 mb-5'>
+          <Button onClick={handleAddToCart} className='w-full bg-yellow-300 mb-5 font-bold'>
             Add to Cart
           </Button>
-          <div>Your cart: {data.cart}</div>
+          <div className='text-xl'>Your cart: {data.cart}</div>
           { error && <div className='text-red-500 mt-2'>{error}</div> }
         </div>
       </div>
